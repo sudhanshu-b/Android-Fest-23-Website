872597e51417ec86bd909bb3f083a30dd7c7bb10
Sudhanshu Burile <123616951+sudhanshu-b@users.noreply.github.com>
2023-04-09 23:26:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,26 +31,3 @@ var nav = document.getElementById('navlist');
 
     window.addEventListener("resize", windowResizeHandler);
 
-
-
-let bushback = document.getElementById('bushback');
-let bushfront = document.getElementById('bushfront');
-let yccegate = document.getElementById('yccegate');
-let text = document.getElementById('venuetxt');
-
-window.addEventListener('scroll', () => {
-    let value = window.scrollY;
-    let parallaxSection = document.querySelector('.parallax');
-
-    if (parallaxSection.getBoundingClientRect().top <= 0) {
-        bushback.style.bottom = (value - parallaxSection.offsetTop) * -0.5 + 'px';
-        yccegate.style.bottom = (value - parallaxSection.offsetTop) * -0.15 + 'px';
-        bushfront.style.bottom = (value - parallaxSection.offsetTop) * -0.75 + 'px';
-        text.style.marginTop = (value - parallaxSection.offsetTop) * 2.5 + 'px';
-    } else {
-        bushback.style.bottom = 0;
-        yccegate.style.bottom = 0;
-        bushfront.style.bottom = 0;
-        text.style.marginTop = 0;
-    }
-});
